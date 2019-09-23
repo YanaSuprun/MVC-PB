37eb4b254eb2b863dca50465b689b83dc03b25d5
YanaSuprun <yana.ysuprun@gmail.com>
2019-09-23 20:37:55 +0300

    Markup changed

--- app/models/ModelExchange.js
+++ app/models/ModelExchange.js
@@ -2,11 +2,11 @@ export class ModelExchange {
   constructor(contr) {
     this.controller = contr;
     this.api = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5';
-  };
+  }
 
   getRate(){
     fetch(this.api).then(data => data.json()).then((arr) => {
       this.controller.getRate(arr);
     });
-  };
-};
+  }
+}
